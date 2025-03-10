1b9d6b2171c2ebad1eedad7270d023503383d8af
febo2591 <felix.boehning@tu-ilmenau.de>
2025-03-10 14:58:41 +0000

    Refactor Datahandle class to improve initialization and logging; remove unused code

--- datahandle.py
+++ datahandle.py
@@ -8,53 +8,39 @@ from typing import Union
 from labtools.labhandler import Labhandler
 from labtools.directory_utils import *
 
-# class Filehandle(object):
-
-#     def __init__(
-#             self,
-#             ref=None,
-#             label:str=None,
-#             remarks:str=None,
-#             labh=Labhandler(),
-#             **kwargs):
-    
-
-    
-
 
 class Datahandle(object):
+    logger=logging.getLogger(__name__)
 
     def __init__(
             self,
             ref=None,
             label:str=None,
             remarks:str=None,
-            df:pd.DataFrame=None,
-            labh=Labhandler(),
+            data_df:pd.DataFrame=None,
+            labh=Labhandler,
             **kwargs):
-
-        self.remarks=remarks
+        
+        self.logger.debug(f"{self.__dict__=}")
+        
+        self.label = label
+        self.remarks = remarks
 
         if labh is not None:
-            self.labh=labh
-            self.labh.attach_parent(locals())
-
+            self.labh=labh(locals())
+            #self.labh.attach_parent(locals())
+            data_df=self.labh.handle_object(locals(),'data_df', save_file=True, overwrite=False)
         
-        if isinstance(df, pd.DataFrame):
-            self.df = df.copy(); del df
+        if isinstance(data_df, pd.DataFrame):
+            self.data_df = data_df.copy(); del data_df
 
-        elif Path(f"{self._path}/df.pkl").exists():
-            self.df = pd.read_pickle(f"{self._path}/df.pkl")
+        # elif Path(f"{self._path}/df.pkl").exists():
+        #     self.df = pd.read_pickle(f"{self._path}/df.pkl")
         
-        if hasattr(self, 'df'):
-            self.n_rows = len(self.df)
-            self.columns = ', '.join(list(self.df.columns))
         
-
-    
-    def save(self):
-        self.save_config()
-        self.df.to_pickle(f"{self._path}/df.pkl")
+    # def save(self):
+    #     self.labh.save()
+    #     self.df.to_pickle(f"{self._path}/df.pkl")
         
 
 
